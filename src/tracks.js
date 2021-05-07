02a358c53d1489914105e94e69d8693d8b4bb275
sly18Peso4all <jomasco18@gmail>
2021-05-07 11:03:57 +0100

    first commit

---
+++ src/tracks.js
@@ -0,0 +1,30 @@
+import imgSrc from "./assets/artwork.jpeg";
+import imgSrc2 from "./assets/artwork2.jpeg";
+import imgSrc3 from "./assets/artwork3.jpeg";
+import cali from "./assets/cali.mp3";
+import nightlife from "./assets/nightlife.mp3";
+import techhouse from "./assets/tech-house-loop.mp3";
+
+export default [
+  {
+    title: "Cali",
+    artist: "Wataboi",
+    audioSrc: cali,
+    image: imgSrc,
+    color: "#00aeb0",
+  },
+  {
+    title: "nightlife",
+    artist: "MichaelKobrin",
+    audioSrc: nightlife,
+    image: imgSrc2,
+    color: "#ffb77a",
+  },
+  {
+    title: "TechHouse",
+    artist: "ZenMan",
+    audioSrc: techhouse,
+    image: imgSrc3,
+    color: "#5f9fff",
+  },
+];
